ce75f80172fb7b26d65855bd2b26546b17d78ca7
Halvadar <Levanbaidoshvili@gmail.com>
2021-06-28 20:41:50 +0400

    Title and Icon added.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Head from "next/head";
 import Header from "../components/Header";
 
 interface indexProps {}
 
 const Index: React.FunctionComponent<indexProps> = ({}) => {
-  return <Header />;
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <link rel="shortcut icon" href="/Sun.svg" type="image/x-icon" />
+      </Head>
+      <Header />;
+    </>
+  );
 };
 
 export default Index;
